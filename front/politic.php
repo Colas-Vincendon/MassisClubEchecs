763540fab89a83dd4d8755245508486f6e05f8ca
Colas <colasvincendon@gmail.com>
2023-07-24 23:38:37 +0200

    contact.php et politic.php

--- front/politic.php
+++ front/politic.php
@@ -8,16 +8,12 @@
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
     />
-    <link rel="stylesheet" href="../front/css/header.css" />
     <link rel="stylesheet" href="../front/css/politic.css" />
-    <link rel="stylesheet" href="../front/css/coordonnees.css" />
   </head>
 
   <body>
     <?php
-require_once '../front/components/header.html';
 require_once '../front/components/politic.html';
-require_once '../front/components/coordonnees.html';
     ?>
     <script
       src="https://kit.fontawesome.com/9468c33ba3.js"
