2f066e4825179d0a4abf2b0439513c7601630bb4
Colas <colasvincendon@gmail.com>
2023-07-26 16:34:12 +0200

    contact.php et politic.php

---
+++ front/staff.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Massis Club d'Echecs</title>
+    <link
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
+      rel="stylesheet"
+    />
+    <link rel="stylesheet" href="../front/css/headerVieDuClub.css" />
+    <link rel="stylesheet" href="../front/css/staff.css" />
+    <link rel="stylesheet" href="../front/css/coordonnees.css" />
+    <link rel="stylesheet" href="../front/css/footerCopyright.css" />
+  </head>
+
+  <body>
+    <?php
+require_once '../front/components/headerVieDuClub.html';
+require_once '../front/components/staff.html';
+require_once '../front/components/coordonnees.html';
+require_once '../front/components/footerCopyright.html';
+    ?>
+    <script
+      src="https://kit.fontawesome.com/9468c33ba3.js"
+      crossorigin="anonymous"
+    ></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="../front/script/colorPhoto.js"></script>
+    <script src="../front/script/clickOutsideDropdown.js"></script>
+    <script src="../front/script/clickOutsideNavbarCollapse.js"></script>
+  </body>
+</html>
