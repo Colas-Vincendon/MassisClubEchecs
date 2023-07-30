20dc5d6c3345fe4350e470c2fb763331864c55bc
Colas <colasvincendon@gmail.com>
2023-07-30 21:00:09 +0200

    meta

--- front/actualites.php
+++ front/actualites.php
@@ -3,7 +3,16 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Massis Club d'Echecs</title>
+    <title>Massis Club Echecs</title>
+    <meta name="description" content="Le Massis Club d'Échecs est situé dans le 3ème arrondissement de Lyon. Que vous soyez débutant dans le monde des échecs ou un maître du jeu à la recherche de nouveaux défis, le club offre une plateforme pour tous. Le Massis Club d'Échecs est plus qu'un lieu de jeu : c'est une communauté où amateurs et professionnels se rencontrent, partagent et cultivent leur passion pour ce jeu intemporel. Venez nous rejoindre et laissez-vous immerger dans l'art fascinant des échecs.">
+<link rel="shortcut icon" href="../medias/logoClubCarre.jpg">
+    <meta property="og:title" content="Massis Club Echecs">
+    <meta property="og:description" content="Le Massis Club d'Échecs est situé dans le 3ème arrondissement de Lyon. Que vous soyez débutant dans le monde des échecs ou un maître du jeu à la recherche de nouveaux défis, le club offre une plateforme pour tous. Le Massis Club d'Échecs est plus qu'un lieu de jeu : c'est une communauté où amateurs et professionnels se rencontrent, partagent et cultivent leur passion pour ce jeu intemporel. Venez nous rejoindre et laissez-vous immerger dans l'art fascinant des échecs.">
+<meta property="og:url" content="https://massis-club-echecs.com/front/actualites">
+<meta property="og:image" content="https://club-echecs-61966ca93fc8.herokuapp.com/medias/logoClub.png">
+<meta property="og:type" content="website">
+<meta property="og:site_name" content="Massis Club Echecs">
+<meta property="og:locale" content="fr_FR">
     <link
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css"
       rel="stylesheet"
--- front/index.php
+++
@@ -1,2 +0,0 @@
-<?php
-header('location: ./front/accueil.php');
