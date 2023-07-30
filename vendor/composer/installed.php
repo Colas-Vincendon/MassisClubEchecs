cbb3de7c75a5ee26473ca48c9a2a107f8b2cf754
Colas <colasvincendon@gmail.com>
2023-07-31 00:09:02 +0200

    meta

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'colas/mce',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'ba4f0b08ca507b16fabd6d23ee0fb0229ef57d70',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'colas/mce' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'ba4f0b08ca507b16fabd6d23ee0fb0229ef57d70',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
