ecb9691dee9591b4c3e344121ce998fca6540458
Colas <colasvincendon@gmail.com>
2023-07-31 16:43:35 +0200

    meta

--- back/sendEmail.php
+++ back/sendEmail.php
@@ -36,9 +36,9 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
             // Envoyer l'e-mail
             if (mail($destinataire, $sujet, $contenu, $headers)) {
-                header('location: ../front/successEmail.php');
+                header('location: ../front/successEmail');
             } else {
-                header('location: ../front/failEmail.php');
+                header('location: ../front/failEmail');
             }
         }
     }
